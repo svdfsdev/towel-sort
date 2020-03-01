75c63510894fc90796f61c24c403fed1e39b956b
svdfsdev <58512937+svdfsdev@users.noreply.github.com>
2020-03-01 23:54:49 +0300

    feat: added implementation

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix) {
+
+	if ( typeof(matrix) == "undefined" || matrix.length == 0) {
+		return [];
+	} else {
+		
+		let arr = [];
+
+		for (let i = 0; i < matrix.length; i++) {
+
+			if (i % 2 == 0) {
+				arr = arr.concat(matrix[i]);
+			} else {
+				arr = arr.concat(matrix[i].reverse());
+			}
+	
+		}
+		return arr;
+	}
+
 }
